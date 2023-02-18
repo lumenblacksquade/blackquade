d322a9ce5eb387bb58c5e1ddc2000e214e202dc0
lumenblacksquade <125724435+lumenblacksquade@users.noreply.github.com>
2023-02-18 12:31:44 +0530

    Add files via upload
    commit2

--- index.php
+++ index.php
@@ -6,18 +6,4 @@
 </head>
 <body>
 <divclass="logincontent"align="center">
-<divclass="logintop">
-<h1>Login Page</h1>
-</div></br></br></br></br>
-<divclass="loginbottom">
-<formaction="{​​​​​​​​{​​​​​​​​ url_for('login')}​​​​​​​​}​​​​​​​​"method="post"autocomplete="off">
-<divclass="msg">{​​​​​​​​{​​​​​​​​ msg }​​​​​​​​}​​​​​​​​</div>
-<inputtype="text"name="username"placeholder="Enter Your Username"class="textbox"id="username"required></br></br>
-<inputtype="password"name="password"placeholder="Enter Your Password"class="textbox"id="password"required></br></br></br>
-<inputtype="submit"class="btn"value="Login">
-</form></br></br>
-<pclass="worddark">New to this page? <aclass="worddark"href="{​​​​​​​​{​​​​​​​​ url_for('register')}​​​​​​​​}​​​​​​​​">Register here</a></p>
-</div>
-</div>
-</body>
-</html>
+<divclass="lo
